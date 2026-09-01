144be65f77dc2af4ae0f65dba1078243a55d4493
quenrythane <artur.babinski.g@gmail.com>
2026-09-01 20:57:49 +0200

    stworzenie pierwszego testu z pytest

---
+++ pytest_cwiczenia/test_kalkulator.py
@@ -0,0 +1,11 @@
+# import kalkulator
+from kalkulator import dodaj
+
+# wynik = dodaj(1, 2)
+# print(wynik)
+
+def test_dodaj_1():
+    assert dodaj(1, 2) == 3
+
+def test_dodaj_2():
+    assert dodaj(1, 4) == 5
