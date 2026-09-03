ba1699985e94ec2fd841e617177750f3f0a80bc2
quenrythane <artur.babinski.g@gmail.com>
2026-09-03 19:47:24 +0200

    paramtryzacja testów

--- pytest_cwiczenia/test_kalkulator.py
+++ pytest_cwiczenia/test_kalkulator.py
@@ -8,9 +8,13 @@ def test_dodaj_1():
     assert dodaj(1, 4) == 5
 
 
+# pytest -m odejmij <- uruchamia test z mark.odejmowanie
 @pytest.mark.odejmij
-def test_odejmowanie_1():
-    assert odejmowanie(5, 2) == 3
-    assert odejmowanie(5, 10) == -5
-    assert odejmowanie(5, 5) == 0
-
+@pytest.mark.parametrize("liczba_1, liczba_2, wynik", [
+    (5, 2, 3),
+    (5, 10, -5),
+    (5, 5, 0),
+    (1, 1, 0),
+])
+def test_odejmowanie_1(liczba_1, liczba_2, wynik):
+    assert odejmowanie(liczba_1, liczba_2) == wynik
