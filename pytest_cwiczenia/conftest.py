e8998d8650788f7f6fbebc7c101a446cb3e652b1
quenrythane <artur.babinski.g@gmail.com>
2026-09-03 19:27:49 +0200

    dodano scope i autouse do fixtures

--- pytest_cwiczenia/conftest.py
+++ pytest_cwiczenia/conftest.py
@@ -1,14 +1,21 @@
 import pytest
 
 
-@pytest.fixture
+@pytest.fixture(autouse=True)
 def funkcja_pomocnicza():
     print("\n Start testu")
     yield
     print("\n Koniec testu")
 
 
+@pytest.fixture(scope="session", autouse=True)
+def funkcja_pomocnicza_sesji():
+    print("\n Start sesji testowej")
+
+
+
 @pytest.fixture
 def base_url():
     url = "http://www.google.com"
     return url
+
--- pytest_cwiczenia/test_kalkulator.py
+++ pytest_cwiczenia/test_kalkulator.py
@@ -1,12 +1,12 @@
 from kalkulator import dodaj, odejmowanie
 
 
-def test_dodaj_1(funkcja_pomocnicza):
+def test_dodaj_1():
     assert dodaj(1, 2) == 3
     assert dodaj(1, 4) == 5
 
 
-def test_odejmowanie_1(funkcja_pomocnicza):
+def test_odejmowanie_1():
     assert odejmowanie(5, 2) == 3
     assert odejmowanie(5, 10) == -5
     assert odejmowanie(5, 5) == 0
