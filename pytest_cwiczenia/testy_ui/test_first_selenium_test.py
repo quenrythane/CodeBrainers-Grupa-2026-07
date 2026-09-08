736de0259fceafd9852efc0650bcfcaa449f9bf5
quenrythane <artur.babinski.g@gmail.com>
2026-09-08 20:45:42 +0200

    first login "test"

--- pytest_cwiczenia/testy_ui/test_first_selenium_test.py
+++ pytest_cwiczenia/testy_ui/test_first_selenium_test.py
@@ -1,4 +1,10 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By  # Pozwala szukać elementów strony
+from selenium.webdriver.common.keys import Keys  # Pozwala wpisywać klawiaturą teksty do pól
+
+# id="username"
+# id="password"
+# type="submit"
 
 driver = webdriver.Chrome()
 URL = "http://127.0.0.1:8000"
@@ -6,3 +12,14 @@ driver.get(URL)
 
 print(driver.title)
 input("Wciśnij ENTER, aby zamknąć przeglądarkę")
+
+
+driver.find_element(By.ID, "username").send_keys("admin")
+driver.find_element(By.ID, "password").send_keys("admin")
+
+input("2 Wciśnij ENTER, aby zamknąć przeglądarkę")
+
+driver.find_element(By.CSS_SELECTOR, "button[type='submit']").click()
+
+input("3 Wciśnij ENTER, aby zamknąć przeglądarkę")
+
