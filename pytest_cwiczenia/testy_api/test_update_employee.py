356e6611d99bb1b3ee97722aef0ba84ba862acf9
quenrythane <artur.babinski.g@gmail.com>
2026-09-08 19:57:13 +0200

    update assertion

--- pytest_cwiczenia/testy_api/test_update_employee.py
+++ pytest_cwiczenia/testy_api/test_update_employee.py
@@ -10,9 +10,16 @@ def test_update_employee(base_url, auth_headers, employee_update_data):
 
     # Assert
     response_body = response.json()
+
+    # asercje linijka pod linijką (coś jak pisanie 10 razy print("text"))
     assert response.status_code == 200
     assert response_body["name"] == "Damian"
     assert response_body["salary"] == 4000
     assert response_body["age"] == 40
     assert response_body["position"] == "Senior QA"
     assert response_body["on_leave"] == False
+
+
+    # asercje w pętli - zamiast pisać 10 razy print("text"), piszę raz pętlę która wyprintuje 10 razy za mnie
+    for key, value in employee_update_data.items():
+        assert response_body[key] == value
