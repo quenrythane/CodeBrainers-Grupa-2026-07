07312fc927b31ae62c513fd886d1f35c6bcf5126
quenrythane <artur.babinski.g@gmail.com>
2026-09-08 19:55:06 +0200

    refactor delete-request and add comments

--- pytest_cwiczenia/testy_api/test_delete_employee.py
+++ pytest_cwiczenia/testy_api/test_delete_employee.py
@@ -5,19 +5,19 @@ import pytest
 @pytest.mark.delete_employee
 def test_delete_employee(base_url, auth_headers, employee_data):
 
-    # Arrange - najpierw tworzymy pracownika, aby go usunąć
+    # Arrange - najpierw tworzymy pracownika, którego będziemy mogli usunąć
     create_response = requests.post(f"{base_url}/employees", headers=auth_headers, json=employee_data)
     created_employee = create_response.json()
-    employee_id = created_employee["id"]
+    employee_id = created_employee["id"]  # po stworzeniu pracownika pobieramy jego id żeby móc usunąć pracownika o tym konkretnym id
 
     # Act
-    response = requests.delete(f"{base_url}/employees/{employee_id}", headers=auth_headers)
+    response = requests.delete(f"{base_url}/employees/{employee_id}", headers=auth_headers)  # usuwamy pracownika
 
     # Assert
     assert response.status_code == 200
 
     # Weryfikacja czy pracownik rzeczywiście został usunięty z listy
-    get_all_response = requests.get(f"{base_url}/employees", headers=headers)
+    get_all_response = requests.get(f"{base_url}/employees", headers=auth_headers)
     assert get_all_response.status_code == 200
-    employees_ids = [emp["id"] for emp in get_all_response.json()]
-    assert employee_id not in employees_ids
+    employees_ids = [emp["id"] for emp in get_all_response.json()]  # lista wszystkich id pracowników
+    assert employee_id not in employees_ids  # sprawdzamy czy id usuniętego pracownika nie ma na liście pracowników
