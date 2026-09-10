caa6878ddebffa43162ca15fdd8b4944c932510c
quenrythane <artur.babinski.g@gmail.com>
2026-09-10 20:17:57 +0200

    przywrócenie testu w wersji bez pom i w wersji z pom dla lepszego porówniania

---
+++ pytest_cwiczenia/testy_ui/test_selenium_with_pom.py
@@ -0,0 +1,27 @@
+import pytest
+from pageObjects.loginPage import LoginPage
+
+
+
+@pytest.mark.ui_login_pom
+def test_ui_login_pom(driver, wait):
+    ## Arrange
+    """ Pobieramy wszystkie "technikalia" z klasy LoginPage """
+    loginPage = LoginPage(driver, wait)
+
+
+    ## Act
+    """ Dzięki temu że wszystkie technikalia są schowane w klasie LoginPage, to test wygląda super prosto i elegancko """
+    loginPage.open()
+    loginPage.enter_username("admin")
+    loginPage.enter_password("admin")
+    loginPage.click_submit_button()
+
+    ## Assert
+    """ logika asercji jest w LoginPage (to jest dyskusyjne - jeden tester powie że tak się robi, inny że tak się nie robi)
+    Sam bym powiedział że tak się NIE robi, ale w tym przypadku takie użycie zwiększało czytelnosć"""
+    assert loginPage.is_form_card_displayed()
+
+
+
+
